0a7a1bf2b30c64e1b1f2102248fe162d381f2930
unknown <varikvi@yahoo.com>
2025-06-07 01:29:15 +0200

    feature:section04: adding images, preparing scene

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,8 +1,13 @@
 // create stage
+const addedH = 50;
+const proport = 2;
+const originalImgSize = 1200;
+
 const stage = new Konva.Stage({
   container: 'container',
-  width: 578,
-  height: 530,
+  width: originalImgSize/proport,
+  height: originalImgSize/proport + addedH,
+
 });
 
 const background = new Konva.Layer();
@@ -10,56 +15,37 @@ const animalLayer = new Konva.Layer();
 const animalShapes = [];
 let score = 0;
 
-// create and load background image
-const backgroundImage = new Image();
-backgroundImage.onload = function() {
-  const backgroundKonvaImage = new Konva.Image({
-    image: backgroundImage,
-    x: 0,
-    y: 0,
-    width: stage.width(),
-    height: stage.height(),
-  });
-  background.add(backgroundKonvaImage);
-  backgroundKonvaImage.moveToBottom();
-};
-backgroundImage.src = '../assets/img/beach.png';
 
-// image positions
-const animals = {
-  monkey: {
-    x: 275,
-    y: 70,
-  },
-  dino: {
-    x: 2,
-    y: 70,
-  },
-};
 
 const outlines = {
-  monkey_black: {
-    x: 300,
-    y: 420,
-  },
+
  dino_black: {
-    x: 3,
-    y: 420,
+    x: 690/proport,
+    y: 400/proport + addedH,
   },
+   desk_black: {
+    x: 0,
+    y: 470/proport + addedH,
+  },
+   craneo_black: {
+    x: 200/proport,
+    y: 370/proport + addedH,
+  },
+   paper_black: {
+    x: 60/proport,
+    y: 570/proport + addedH,
+  },
+   bone_black: {
+    x: 400/proport,
+    y: 170/proport + addedH,
+  },
+   book_black: {
+    x: 0,
+    y: 430/proport + addedH,
+  },
+
 };
 
-function isNearOutline(animal, outline) {
-  const a = animal;
-  const o = outline;
-  const ax = a.x();
-  const ay = a.y();
-
-  if (ax > o.x - 20 && ax < o.x + 20 && ay > o.y - 20 && ay < o.y + 20) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
 // create message text
 const messageText = new Konva.Text({
@@ -79,9 +65,10 @@ function updateMessage(text) {
   messageText.text(text);
 }
 
+const images = {};
+
 function loadImages(sources, callback) {
   const assetDir = '../assets/img/';
-  const images = {};
   let loadedImages = 0;
   let numImages = 0;
   
@@ -97,77 +84,12 @@ function loadImages(sources, callback) {
       }
     };
     images[src].src = assetDir + sources[src];
-    images[src].width = images[src].width / 3; //this value should depend on existing viewport size
-    images[src].height = images[src].height / 3;
+    images[src].width = images[src].width / proport; //this value should depend on existing viewport size
+    images[src].height = images[src].height / proport;
   }
 }
 
 function initStage(images) {
-  // create draggable animals
-  for (const key in animals) {
-    // anonymous function to induce scope
-    (function () {
-      const privKey = key;
-      const anim = animals[key];
-
-      const animal = new Konva.Image({
-        image: images[key],
-        x: anim.x,
-        y: anim.y,
-        draggable: true,
-      });
-      console.log(animal);
-
-      animal.on('dragstart', function () {
-        this.moveToTop();
-      });
-      
-      /*
-       * check if animal is in the right spot and
-       * snap into place if it is
-       */
-      animal.on('dragend', function () {
-        const outline = outlines[privKey + '_black'];
-        if (!animal.inRightPlace && isNearOutline(animal, outline)) {
-          animal.position({
-            x: outline.x,
-            y: outline.y,
-          });
-          animal.inRightPlace = true;
-
-          if (++score >= 4) {
-            const text = 'You win! Enjoy your booty!';
-            updateMessage(text);
-          }
-
-          // disable drag and drop
-          setTimeout(function () {
-            animal.draggable(false);
-          }, 50);
-        }
-      });
-      
-      // make animal glow on mouseover
-      animal.on('mouseover', function () {
-        animal.image(images[privKey + '_glow']);
-        console.log(images[privKey]);
-        document.body.style.cursor = 'pointer';
-      });
-      
-      // return animal on mouseout
-      animal.on('mouseout', function () {
-        animal.image(images[privKey]);
-        document.body.style.cursor = 'default';
-      });
-
-      animal.on('dragmove', function () {
-        document.body.style.cursor = 'pointer';
-      });
-
-      animalLayer.add(animal);
-      animalShapes.push(animal);
-    })();
-  }
 
   // create animal outlines
   for (const key in outlines) {
@@ -195,13 +117,13 @@ function initStage(images) {
 }
 
 const sources = {
-  beach: 'beach.png',
-//   monkey: 'monkey.png',
-//   monkey_glow: 'monkey-glow.png',
-//   monkey_black: 'monkey-black.png',
-  dino: 'dinoheadtransparent.png',
-  dino_glow: 'dinoheadtransparent.png',
   dino_black: 'dinoheadtransparent.png',
+  book_black: 'booktransparent.png',
+  craneo_black: 'craneotransparent.png',
+  paper_black: 'papertransparent.png',
+  bone_black: 'bonetransparent.png',
+  desk_black: 'desktransparent.png'
+
 };
 
 // Demo warning: You'll need local image files for this demo to work
