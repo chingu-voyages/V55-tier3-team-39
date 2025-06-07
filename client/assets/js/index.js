2e9e7d2519a300cccbf690e5cf3be33fc6ad7a1a
unknown <varikvi@yahoo.com>
2025-06-07 17:16:55 +0200

    feature:section04: fitting content

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -22,6 +22,7 @@ const outlines = {
  dino_black: {
     x: 690/proport,
     y: 400/proport + addedH,
+    text: `They both count as the most prolific\npaleontologists. Marsh made the \nmost of the contributions, with \nmore than 30 new species of dinosaurs.\n Although Cope only counted 3 valid classifications,\n he was better known for contributions to ichtiology and herpetology.`,
   },
    desk_black: {
     x: 0,
@@ -30,26 +31,39 @@ const outlines = {
    craneo_black: {
     x: 200/proport,
     y: 370/proport + addedH,
+    text: `Both had distorded views about the evolution\nof humans. Marsh took bones from native american graves\nto verify if they were less evolved that white men.\nCope (and probably Marsh too) believed that brain size was\nrelated to intelligence. Actually, before dying he donated\nhis body to science not without challenging Marsh to do the same,\nto let others measure their brains and prove who was the most intelligent of both.\n\nMarsh didn't do that.`,
   },
    paper_black: {
     x: 60/proport,
     y: 570/proport + addedH,
+    text: `Cope was the most prolifc author, with around 1500 papers\nwith vivid descriptions of his findings. Marsh was less prolific but more systematic\nin his descriptions.\nMarsh had an enourmous impact on the scientific development\nof the paleontology, but Cope had a big influence in our view of\nthe pre-historic environment.`,
   },
    bone_black: {
     x: 400/proport,
     y: 170/proport + addedH,
+    text: `One of the bad consequences of the war\n was the number of errors they made while\nrushing their classifications.\nCope was who got it wrong most of the times.\nFrom 30 classifications of "new to science", only 3 are currently valid.\nMarsh made 58 classifications but got 20 wrong.`,
   },
    book_black: {
     x: 0,
     y: 430/proport + addedH,
+    text:``
   },
 
 };
 
+const facts = {
+    6: `Marsh and Cope count as the most prolific\npaleontologists of the XIX century. Marsh made the \nmost of the contributions, with \nmore than 30 new species of dinosaurs.\n Although Cope only counted 3 valid \nclassifications, he was better known \nfor contributions \nto ichtiology and herpetology.`,
+    7: ``,
+    8: `Both had distorded views about the evolution\nof humans. Marsh took bones from native american graves\nto verify if they were less evolved that white men.\nCope (and probably Marsh too) believed that brain size was\nrelated to intelligence. Actually, before dying he donated\nhis body to science not without challenging Marsh to do the same,\nto let others measure their brains and prove who was the most intelligent of both.\n\nMarsh didn't do that.`,
+    9: `Cope was the most prolifc author, with around 1500 papers\nwith vivid descriptions of his findings. Marsh was less prolific but more systematic\nin his descriptions.\nMarsh had an enourmous impact on the scientific development\nof the paleontology, but Cope had a big influence in our view of\nthe pre-historic environment.`,
+    10: `One of the bad \nconsequences of the\n war was the number of \nerrors they made while\nrushing their\nclassifications. Cope \nwas who got it wrong\nmost of the times.\nFrom 30 "new to \nscience" classifications,\n only 3 are currently \nvalid.\nMarsh made 58\nclassifications but got\n    20 wrong.`,
+    11: ``
+}
+
 
 // create message text
 const messageText = new Konva.Text({
-  text: 'Ahoy! Put the animals on the beach!',
+  text: 'BONE WARS FACTS\n\n(click on the objects in the office)',
   x: stage.width() / 2,
   y: 40,
   fontSize: 20,
@@ -61,8 +75,25 @@ const messageText = new Konva.Text({
 });
 background.add(messageText);
 
+// create message text
+const messageText02 = new Konva.Text({
+  text: '',
+  //x:0,
+  //y: 200,
+  x: 355,
+  y:120,
+  fontSize: 25,
+  fontFamily: 'Calibri',
+  fill: 'white',
+  //align: 'center',
+  // for center align we need to set offset
+  //offsetX: 200,
+});
+
+background.add(messageText02);
+
 function updateMessage(text) {
-  messageText.text(text);
+  messageText02.text(text);
 }
 
 const images = {};
@@ -114,13 +145,15 @@ function initStage(images) {
             let out = animalLayer.children[outIndex];
             if(out._id != outline._id){
                 if(out.opacity() === 1.0){
-                    if((outline._id != 5 && out._id != 6) || outline._id == 5){
+                    if((outline._id != 6 && out._id != 7) || outline._id == 6){
                         out.opacity(0);
                     };
                     out.listening(false);
+                    updateMessage(facts[outline._id]);
                 }else{
                     out.opacity(1.0);
                     out.listening(true);
+                    updateMessage();
                 }
             }
 
@@ -137,9 +170,7 @@ function initStage(images) {
   stage.add(background);
   stage.add(animalLayer);
 
-  updateMessage(
-    'Ahoy! Put the animals on the beach!'
-  );
+  //updateMessage();
 }
 
 const sources = {
